e0b6cb2a872ccb986094a507c58f3d9da340a05a
Hamza Sheikh <zafarhamza789@gmail.com>
2026-01-08 03:08:25 +0100

    05:13:20

--- client/src/Pages/ResumeBuilder.jsx
+++ client/src/Pages/ResumeBuilder.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { useParams, Link } from 'react-router-dom'
 import { dummyResumeData } from '../assets/assets'
 import PersonalInfoForm from '../components/PersonalInfoForm'
-import { ArrowLeftIcon, User, FileText, GraduationCap, Briefcase, FolderIcon, ChevronLeft, ChevronRight } from 'lucide-react'
+import { ArrowLeftIcon, DownloadIcon, EyeIcon, EyeOffIcon, User, FileText, GraduationCap, Briefcase, FolderIcon, ChevronLeft, ChevronRight, Share2Icon } from 'lucide-react'
 import ResumePreview from '../components/ResumePreview.jsx'
 import TemplateSelector from '../components/TemplateSelector'
 import ColorPicker from '../components/ColorPicker.jsx'
@@ -10,6 +10,7 @@ import ProfessionalSummaryForm from '../components/ProfessionalSummaryForm'
 import ExperienceForm from '../components/ExperienceForm'
 import EducationForm from '../components/EducationForm'
 import ProjectForm from '../components/ProjectForm'
+import SkillsForm from '../components/SkillsForm'
 
 const ResumeBuilder = () => {
 
@@ -56,6 +57,26 @@ const ResumeBuilder = () => {
   useEffect(() => {
     loadExistingResume();
   }, [])
+
+
+  const changeResumeVisibility = async () => {
+    setResumeData({ ...resumeData, public: !resumeData.public })
+  }
+
+  const handleShare = () => {
+    const frontEndUrl = window.location.href.split('/app')[0];
+    const resumeUrl = frontEndUrl + '/view' + resumeId;
+
+    if (navigator.share) {
+      navigator.share({ url: resumeUrl, text: "My Resume" })
+    } else {
+      alert('Share not supported on this browser.')
+    }
+  }
+
+  const downloadResume = () => {
+    window.print();
+  }
 
 
   return (
@@ -143,7 +164,16 @@ const ResumeBuilder = () => {
                 {activeSection.id === 'projects' && (
                   <ProjectForm data={resumeData.project} onChange={(data) => setResumeData(prev => ({ ...prev, project: data }))} />
                 )}
+
+                {activeSection.id === 'skills' && (
+                  <SkillsForm data={resumeData.skills} onChange={(data) => setResumeData(prev => ({ ...prev, skills: data }))} />
+                )}
               </div>
+
+              <button className='bg-gradient-to-br from-green-100 to-green-200 ring-green-300 text-green-600
+              ring hover:ring-green-400 transition-all rounded-md px-6 py-2 mt-6 text-sm'>
+                Save Changes
+              </button>
 
 
 
@@ -154,8 +184,28 @@ const ResumeBuilder = () => {
           {/* Right-panel-preview */}
 
           <div className='lg:col-span-7 max-lg:mt-6'>
-            <div>
+            <div className='relative w-full'>
               {/* ---Buttons */}
+
+              <div className='absolute bottom-3 left-0 right-0 flex items-center justify-end gap-2'>
+                {resumeData.public && (
+                  <button onClick={handleShare}
+                    className='flex-item-center p-2 px-4 text-xs bg-gradient-to-br from-blue-100 to-blue-200 text-blue-600
+                  rounded-lg ring-blue-300 hover:ring transition-colors'>
+                    <Share2Icon className="size-4" />
+                  </button>
+                )}
+                <button onClick={changeResumeVisibility} className='flex items-center p-2 px-4 gap-2 text-xs
+                bg-gradient-to-br from-purple-100 to-purple-200 text-purple-600 ring-purple-300
+                 rounded-1g hover:ring transition-colors'>
+                  {resumeData.public ? <EyeIcon className="size-4" /> : <EyeOffIcon className="size-4" />}
+                  {resumeData.public ? 'Public' : 'Private'}
+                </button>
+                <button onClick={downloadResume} className='flex items-center gap-2 px-6 py-2 text-xs bg-gradient-to-br from-green-100 to-green-200 text-green-600 rounded-1g ring-green-300 hover:ring transition-colors'>
+                  <DownloadIcon className='size-4' />
+                  Download
+                </button>
+              </div>
             </div>
             {/* ---Resume-Preview */}
 
